6ede2f2995537c5ba731ac1378186f044ccc6706
jnicolaugraupera <jnicolaugraupera@uoc.edu>
2026-04-22 01:56:15 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hola Mundo desde Java 21 🚀🚀🚀");
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
-        // Mantener el contenedor vivo
-        while (true) {
-            Thread.sleep(1000);
-        }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Hola desde Java en Kubernetes! Pod: " + System.getenv("HOSTNAME");
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+        System.out.println("Servidor escuchando en puerto 8080");
     }
 }

af607df73a698ab9ae3901cc0f2c097cbbc1d96a
jnicolaugraupera <jnicolaugraupera@uoc.edu>
2026-04-22 00:57:44 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hola Mundo desde Java 21 🚀🚀🚀");
+
+        // Mantener el contenedor vivo
+        while (true) {
+            Thread.sleep(1000);
+        }
     }
 }

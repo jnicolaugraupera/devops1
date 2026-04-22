2f7ad920932ec1647a2625bb48d289bbbd793056
jnicolaugraupera <jnicolaugraupera@uoc.edu>
2026-04-22 02:39:18 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -6,16 +8,19 @@ import java.net.InetSocketAddress;
 public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Servidor Java escuchando en puerto 8080");
+    }
 
-        server.createContext("/", exchange -> {
-            String response = "Hola desde Java en Kubernetes! Pod: " + System.getenv("HOSTNAME");
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hola desde Java en Kubernetes 🚀 Pod funcionando!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
-        });
-
-        server.start();
-        System.out.println("Servidor escuchando en puerto 8080!");
+        }
     }
 }
